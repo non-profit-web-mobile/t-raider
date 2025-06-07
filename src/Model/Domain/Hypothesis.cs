f3a7fb24d4e546f174be844f9c8c9f06043858c9
vasilev <vasilev@mindbox.cloud>
2025-06-07 23:42:33 +0300

    fix

--- src/Model/Domain/Hypothesis.cs
+++ src/Model/Domain/Hypothesis.cs
@@ -9,7 +9,7 @@ public record Hypothesis(
 	[property: DataMember(Name = "action")] string Action,
 	[property: DataMember(Name = "probability")] double Probability,
 	[property: DataMember(Name = "explanation")] string Explanation,
-	[property: DataMember(Name = "period")] int Period,
+	[property: DataMember(Name = "period")] string Period,
 	[property: DataMember(Name = "tactics")] string Tactics,
 	[property: DataMember(Name = "stopLoss")] string StopLoss,
 	[property: DataMember(Name = "takeProfit")] string TakeProfit);
