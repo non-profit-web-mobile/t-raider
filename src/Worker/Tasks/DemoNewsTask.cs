d33c47d1431cd902e1920db473bea36599a29a62
ramilyayus <yusupova@mindbox.cloud>
2025-06-08 17:28:10 +0300

    wip

--- src/Worker/Tasks/DemoNewsTask.cs
+++ src/Worker/Tasks/DemoNewsTask.cs
@@ -9,19 +9,18 @@ public class DemoNewsTask(
 	ILogger<DemoNewsTask> logger)
 	: BackgroundTask(TaskSchedule.Periodical(TimeSpan.FromMinutes(1)), logger)
 {
-	private static readonly string[] NewsLinks =
+	private static readonly RawNewsMessage[] NewsLinks =
 	[
-		"https://ria.ru/20210227/investory-1599270790.html",
-		"https://ria.ru/20210227/rassledovanie-1599234947.html",
-		"https://ria.ru/20210227/riski-1599214473.html",
-		"https://ria.ru/20210227/valyuta-1599206472.html",
-		"https://ria.ru/20210227/investory-1599270790.html",
-		"https://ria.ru/20210227/rassledovanie-1599234947.html",
-		"https://ria.ru/20210227/riski-1599214473.html",
-		"https://ria.ru/20210227/valyuta-1599206472.html",
-		"https://ria.ru/20210227/kreditki-1599202110.html",
-		"https://ria.ru/20210227/gosdolg-1599197744.html",
-		"https://ria.ru/20210227/teplo-1599197580.html"
+		new ("https://www.rbc.ru/politics/23/06/2023/6495e7c99a79473ce4258bf6", 9),
+		new ("https://panorama.pub/news/v-vasingtone-startovala-ezegodnaa-ceremonia", 0),
+		new ("https://www.rbc.ru/finances/06/06/2025/68403e4c9a7947dda98935cf", 9),
+		new ("https://www.tbank.ru/invest/social/profile/Interfax/dca05b8d-8c5e-3f2c-b2a1-cda75d693b46",
+			7),
+		new ("https://www.rbc.ru/quote/news/article/682ec7659a79473670690cb4?from=newsfeed",
+			7),
+		new("https://bcs-express.ru/novosti-i-analitika/o-torgakh-aktsiiami-x5-na-mosbirzhe", 7),
+		new("https://www.rbc.ru/quote/news/article/6762def09a79479ffc4b681d", 9),
+		new("https://www.rbc.ru/quote/news/article/6345719e9a79472282efb0b9", 9)
 	];
 
 	protected override async Task ExecuteIterationAsync(CancellationToken cancellationToken)
@@ -31,11 +30,9 @@ public class DemoNewsTask(
 		var kafkaMessageSerializer = serviceProvider.GetRequiredService<IKafkaMessageSerializer>();
 
 		var random = new Random();
-		var randomNewsLink = NewsLinks[random.Next(NewsLinks.Length)];
-		var rawNewsMessage = new RawNewsMessage(randomNewsLink, random.Next(1, 11));
-
+		var randomNews = NewsLinks[random.Next(NewsLinks.Length)];
 		var topicInfo = topicInfoProvider.GetRawNewsTopicInfo();
-		var serializedMessage = kafkaMessageSerializer.Serialize(rawNewsMessage);
+		var serializedMessage = kafkaMessageSerializer.Serialize(randomNews);
 
 		await kafkaProducer.ProduceAsync(topicInfo, Guid.NewGuid().ToString(), serializedMessage, cancellationToken);
 	}
