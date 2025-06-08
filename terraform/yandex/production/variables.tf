d9da818eca8f1777224f126ab5f03cf2c8ab11e3
Konstantin Shvyriaev <shvyriaev@mindbox.ru>
2025-06-08 10:27:54 +0300

    Updated

---
+++ terraform/yandex/production/variables.tf
@@ -0,0 +1,27 @@
+variable "cloud_id" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "service_account_id" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "postgres_password" {
+  type = string
+}
+
+variable "ssh_authorized_key" {
+  type = string
+}
